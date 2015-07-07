d68e42882d12bfdcf0a7407900d3bc17e20e75df
Alan Plum <me@pluma.io>
2015-07-07 09:17:08 +0200

    Rebuilt.

--- dist/arango.all.js
+++ dist/arango.all.js
@@ -1792,33 +1792,29 @@ module.exports = function once(cb) {
 'use strict';
 module.exports = function (PromiseCtor) {
   if (PromiseCtor === false) {
-    return function (callback) {
-      return { callback: callback || function () {} };
+    return function () {
+      var callback = arguments[0] === undefined ? function () {} : arguments[0];
+
+      return { callback: callback };
     };
   }
 
-  return function (callback) {
+  return function () {
+    var callback = arguments[0] === undefined ? function () {} : arguments[0];
+
     if (!PromiseCtor && typeof Promise !== 'function') {
-      return { callback: callback || function () {} };
+      return { callback: callback };
     }
 
     function defer(resolve, reject) {
+      var errback = callback;
       callback = function (err, res) {
         if (err) reject(err);else resolve(res);
+        if (errback) errback(err, res);
       };
     }
 
-    var cb = callback;
-
     var promise = PromiseCtor ? new PromiseCtor(defer) : new Promise(defer);
-
-    if (cb) {
-      promise.then(function (result) {
-        return cb(null, result);
-      }, function (reason) {
-        return cb(reason);
-      });
-    }
 
     return { callback: callback, promise: promise };
   };
