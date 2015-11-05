5442d624dce7d1256dd3b45d974a56a686fa5bc0
Alan Plum <me@pluma.io>
2015-11-05 18:30:08 +0100

    Add test for database.route

--- test/07-routes.js
+++ test/07-routes.js
@@ -1,8 +1,24 @@
 import {describe, it} from 'mocha';
+import {expect} from 'chai';
+import {Database} from '../src';
+import Route from '../src/route';
 
 describe('Arbitrary HTTP routes', () => {
+  const db = new Database();
   describe('database.route', () => {
-    it('is missing tests');
+    it('returns a Route instance', () => {
+      let route = db.route();
+      expect(route).to.be.an.instanceof(Route);
+    });
+    it('creates a route for the given path', () => {
+      let path = '/hi';
+      let route = db.route(path);
+      expect(route._path).to.equal(path);
+    });
+    it('passes the given headers to the new route', () => {
+      let route = db.route('/hello', {'x-magic': 'awesome'});
+      expect(route._headers).to.have.a.property('x-magic', 'awesome');
+    });
   });
 });
 
