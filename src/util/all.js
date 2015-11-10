0e2e19004328ba1c0b09f55ccad6e3dfc48172c3
Alan Plum <me@pluma.io>
2015-11-10 18:30:35 +0100

    Even more ES6

--- src/util/all.js
+++ src/util/all.js
@@ -5,21 +5,19 @@ export default function all(arr, callback) {
 
   if (arr.length === 0) return callback(null, result);
 
-  function step(i) {
-    return (err, res) => {
-      pending -= 1;
-      if (!err) result[i] = res;
-      if (!called) {
-        if (err) callback(err);
-        else if (pending === 0) {
-          if (result.every(r => r === undefined)) callback(null);
-          else callback(null, result);
-        }
-        else return;
-        called = true;
+  const step = i => (err, res) => {
+    pending -= 1;
+    if (!err) result[i] = res;
+    if (!called) {
+      if (err) callback(err);
+      else if (pending === 0) {
+        if (result.every(r => r === undefined)) callback(null);
+        else callback(null, result);
       }
-    };
-  }
+      else return;
+      called = true;
+    }
+  };
 
   arr.forEach((fn, i) => fn(step(i)));
 }
