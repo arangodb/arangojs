52e9e4c643c519b79d78862d2ff53697fac4d882
Alan Plum <me@pluma.io>
2016-01-26 22:26:58 +0100

    Add Graph API remove method to Graph collections

--- src/graph.js
+++ src/graph.js
@@ -15,6 +15,15 @@ class GraphVertexCollection extends BaseCollection {
   _documentPath(documentHandle) {
     return `document/${this._documentHandle(documentHandle)}`;
   }
+  
+  remove(documentHandle, cb) {
+    const {promise, callback} = this._connection.promisify(cb);
+    this._gharial.delete(
+      documentHandle,
+      (err, res) => err ? callback(err) : callback(null, res.body)
+    );
+    return promise;
+  }
 
   vertex(documentHandle, cb) {
     const {promise, callback} = this._connection.promisify(cb);
@@ -42,6 +51,15 @@ class GraphEdgeCollection extends EdgeCollection {
     this.type = types.EDGE_COLLECTION;
     this.graph = graph;
     this._gharial = this._api.route(`gharial/${this.graph.name}/edge/${this.name}`);
+  }
+  
+  remove(documentHandle, cb) {
+    const {promise, callback} = this._connection.promisify(cb);
+    this._gharial.delete(
+      documentHandle,
+      (err, res) => err ? callback(err) : callback(null, res.body)
+    );
+    return promise;
   }
 
   edge(documentHandle, cb) {
