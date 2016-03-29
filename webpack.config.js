d9409149b2bfe151d9c42216be123cc1dbbff316
Alan Plum <me@pluma.io>
2016-03-29 16:16:05 +0200

    Fix browser aliases

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,6 @@
 const resolve = require('path').resolve
 const webpack = require('webpack')
 
-const alias = {}
-alias[resolve(__dirname, 'src/util/byte-length.js')] = 'utf8-length'
-
 module.exports = {
   entry: resolve(__dirname, 'src/index.js'),
   output: {
@@ -17,9 +14,7 @@ module.exports = {
       {test: /\.json$/, loader: 'json-loader'}
     ]
   },
-  resolve: {alias},
   target: 'web',
-  externals: [],
   plugins: [
     new webpack.DefinePlugin({'process.env': {NODE_ENV: '"production"'}}),
     new webpack.optimize.OccurenceOrderPlugin(true),
