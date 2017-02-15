ad59b6cefba11ed599b8ca0b40655a232350e1f2
Alan Plum <me@pluma.io>
2017-02-15 17:45:17 +0100

    Move submodule out of node_modules

--- src/util/request.js
+++ src/util/request.js
@@ -1,6 +1,8 @@
+console.log('TODO fall back to node driver on failure')
 try {
   module.exports = require('./request.fuerte')
 } catch (e) {
   console.warn('Failed to load fuerte driver')
-  module.exports = require('./request.node')
+  throw e
+  // module.exports = require('./request.node')
 }
