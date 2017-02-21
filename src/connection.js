d79fab5d7abbc6a1bacdeafacf5dd4ff291e8704
Alan Plum <me@pluma.io>
2017-02-21 13:42:50 +0100

    Fix error when content-type is missing

--- src/connection.js
+++ src/connection.js
@@ -121,7 +121,7 @@ export default class Connection {
         if (err) callback(err)
         else {
           const rawBody = res.body
-          if (this._baseUrl.protocol !== 'vst:' && res.headers['content-type'].match(MIME_JSON)) {
+          if (this._baseUrl.protocol !== 'vst:' && res.headers['content-type'] && res.headers['content-type'].match(MIME_JSON)) {
             try {
               if (expectBinary) res.body = res.body.toString('utf-8')
               res.body = res.body ? JSON.parse(res.body) : undefined
