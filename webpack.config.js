91fb889e0142890fc0d89290b2be10904cc91972
Alan Plum <me@pluma.io>
2017-12-14 19:16:31 +0100

    Fix browser build

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,56 @@
-'use strict'
-var resolve = require('path').resolve
-var webpack = require('webpack')
+"use strict";
+var resolve = require("path").resolve;
+var webpack = require("webpack");
 
 module.exports = {
-  entry: resolve(__dirname, 'lib/index.js'),
+  entry: resolve(__dirname, "src/index.ts"),
+  devtool: "source-map",
   output: {
-    path: __dirname,
-    filename: 'arangojs.min.js',
-    library: 'arangojs',
-    libraryTarget: 'umd'
+    path: resolve(__dirname, "lib"),
+    filename: "browser.js",
+    library: "arangojs",
+    libraryTarget: "umd"
   },
   module: {
     rules: [
+      // NOTE: these rules apply in reverse order
       {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        include: /node_modules/,
+        test: /\.(ts|js)$/,
+        loader: "babel-loader",
+        options: {
+          presets: [
+            [
+              "babel-preset-env",
+              {
+                target: {
+                  browsers: ["last 2 versions", "ie >= 11"]
+                }
+              }
+            ]
+          ],
+          plugins: [
+            "babel-plugin-transform-class-properties",
+            "babel-plugin-transform-object-rest-spread"
+          ]
+        }
+      },
+      {
+        test: /\.ts/,
+        loader: "ts-loader",
+        options: { transpileOnly: true }
       }
     ]
   },
+  resolve: {
+    extensions: [".web.js", ".web.ts", ".js", ".ts", ".json"]
+  },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env': {NODE_ENV: '"production"'}
+      "process.env": { NODE_ENV: '"production"' }
     }),
     new webpack.optimize.UglifyJsPlugin({
       minimize: true,
-      output: {comments: false}
+      output: { comments: false }
     })
   ]
-}
+};
