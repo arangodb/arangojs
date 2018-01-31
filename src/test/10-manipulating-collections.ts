1d87b77294a3d9e79ae121705f63f7a429e31825
mpv1989 <mark@arangodb.com>
2018-01-31 14:49:36 +0100

    Add collection test

--- src/test/10-manipulating-collections.ts
+++ src/test/10-manipulating-collections.ts
@@ -1,26 +1,64 @@
-describe("Manipulating collections", () => {
-  describe("collection.create", () => {
-    it("is missing tests");
+import {Database} from '../arangojs';
+import {expect} from 'chai';
+
+const ARANGO_VERSION = Number(process.env.ARANGO_VERSION || 30000);
+
+describe('Manipulating collections', () => {
+  let name = `testdb_${Date.now()}`;
+  let db: Database;
+  before(done => {
+    db = new Database({
+      url: process.env.TEST_ARANGODB_URL || 'http://localhost:8529',
+      arangoVersion: ARANGO_VERSION
+    });
+    db
+      .createDatabase(name)
+      .then(() => {
+        db.useDatabase(name);
+        done();
+      })
+      .catch(done);
   });
-  describe("collection.load", () => {
-    it("is missing tests");
+  after(done => {
+    db.useDatabase('_system');
+    db
+      .dropDatabase(name)
+      .then(() => void done())
+      .catch(done);
   });
-  describe("collection.unload", () => {
-    it("is missing tests");
+  describe('collection.create', () => {
+    it('create a new document collection', done => {
+      const collection = db.collection(`c_${Date.now()}`);
+      collection
+        .create()
+        .then(() => {
+          db.collection(collection.name).get().then(bla => {
+            expect(bla).to.have.property("name")
+          })
+        })
+        .then(() => void done())
+        .catch(done);
+    });
   });
-  describe("collection.setProperties", () => {
-    it("is missing tests");
+  describe('collection.load', () => {
+    it('is missing tests');
   });
-  describe("collection.rename", () => {
-    it("is missing tests");
+  describe('collection.unload', () => {
+    it('is missing tests');
   });
-  describe("collection.rotate", () => {
-    it("is missing tests");
+  describe('collection.setProperties', () => {
+    it('is missing tests');
   });
-  describe("collection.truncate", () => {
-    it("is missing tests");
+  describe('collection.rename', () => {
+    it('is missing tests');
   });
-  describe("collection.drop", () => {
-    it("is missing tests");
+  describe('collection.rotate', () => {
+    it('is missing tests');
+  });
+  describe('collection.truncate', () => {
+    it('is missing tests');
+  });
+  describe('collection.drop', () => {
+    it('is missing tests');
   });
 });
