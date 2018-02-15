30bd147c7ecd08b2554f2579d8804d5239f05efc
Alan Plum <me@pluma.io>
2018-02-15 13:04:21 +0100

    Fix broken export
    The es6-error module does something weird with its exports, loading differently in arangojs itself and angular-cli. Rather than export it directly, we need to mess with its default export to make this work in all environments.

--- src/util/error.ts
+++ src/util/error.ts
@@ -1,1 +1,3 @@
-export default require("es6-error") as typeof Error;
+let ExtendableError = require("es6-error");
+ExtendableError = ExtendableError.default || ExtendableError;
+export default ExtendableError as typeof Error;
