cc3aa53d459f5ba51c023b5a9eab8ffeef94ee6a
mpv1989 <mark@arangodb.com>
2018-03-19 13:26:39 +0100

    Add collection tests

--- src/test/09-collection-metadata.ts
+++ src/test/09-collection-metadata.ts
@@ -1,20 +1,91 @@
+import { Database } from "../arangojs";
+import { DocumentCollection } from "../collection";
+import { expect } from "chai";
+
 describe("Collection metadata", () => {
+  let db: Database;
+  let dbName = `testdb_${Date.now()}`;
+  let collection: DocumentCollection;
+  let collectionName = `collection-${Date.now()}`;
+  before(done => {
+    db = new Database({
+      url: process.env.TEST_ARANGODB_URL || "http://localhost:8529",
+      arangoVersion: Number(process.env.ARANGO_VERSION || 30000)
+    });
+    db.createDatabase(dbName).then(() => {
+      db.useDatabase(dbName);
+      collection = db.collection(collectionName);
+      collection
+        .create()
+        .then(() => void done())
+        .catch(done);
+    });
+  });
+  after(done => {
+    db.useDatabase("_system");
+    db
+      .dropDatabase(dbName)
+      .then(() => void done())
+      .catch(done);
+  });
   describe("collection.get", () => {
-    it("is missing tests");
+    it("should return information about a collection", done => {
+      collection
+        .get()
+        .then(info => {
+          expect(info).to.have.property("name", collectionName);
+          expect(info).to.have.property("isSystem", false);
+          expect(info).to.have.property("status", 3); // loaded
+          expect(info).to.have.property("type", 2); // document collection
+        })
+        .then(() => done())
+        .catch(done);
+    });
+    it("should throw if collection does not exists", done => {
+      db
+        .collection("no")
+        .get()
+        .then(done)
+        .catch(err => {
+          expect(err).to.have.property("errorNum", 1203);
+          done();
+        });
+    });
   });
   describe("collection.properties", () => {
-    it("is missing tests");
+    it("should return properties of a collection", done => {
+      collection
+        .properties()
+        .then(properties => {
+          expect(properties).to.have.property("name", collectionName);
+          expect(properties).to.have.property("waitForSync", false);
+        })
+        .then(() => done())
+        .catch(done);
+    });
   });
   describe("collection.count", () => {
-    it("is missing tests");
-  });
-  describe("collection.figures", () => {
-    it("is missing tests");
+    it("should return information about a collection", done => {
+      collection
+        .count()
+        .then(info => {
+          expect(info).to.have.property("name", collectionName);
+          expect(info).to.have.property("count", 0);
+        })
+        .then(() => done())
+        .catch(done);
+    });
   });
   describe("collection.revision", () => {
-    it("is missing tests");
-  });
-  describe("collection.checksum", () => {
-    it("is missing tests");
+    it("should return information about a collection", done => {
+      collection
+        .revision()
+        .then(info => {
+          expect(info).to.have.property("name", collectionName);
+          expect(info).to.have.property("revision");
+        })
+        .then(() => done())
+        .catch(done);
+    });
   });
 });
