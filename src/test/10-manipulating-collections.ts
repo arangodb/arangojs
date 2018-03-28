b2c1a2f04c1666fc8bd1d070a06e37c8f24a09cf
mpv1989 <mark@arangodb.com>
2018-03-28 11:55:52 +0200

    Fix test for cluster setup

--- src/test/10-manipulating-collections.ts
+++ src/test/10-manipulating-collections.ts
@@ -4,7 +4,10 @@ import { expect } from "chai";
 
 const ARANGO_VERSION = Number(process.env.ARANGO_VERSION || 30000);
 
-describe("Manipulating collections", () => {
+describe("Manipulating collections", function() {
+  // create database takes 11s in a standard cluster
+  this.timeout(20000);
+
   let name = `testdb_${Date.now()}`;
   let db: Database;
   let collection: DocumentCollection;
