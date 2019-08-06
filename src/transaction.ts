a9a0f547c310e5027494d1575ebdf4b058ff74fd
Alan Plum <me@pluma.io>
2019-08-06 16:30:40 +0200

    Stream transactions

---
+++ src/transaction.ts
@@ -0,0 +1,51 @@
+import { Connection } from "./connection";
+
+interface TransactionStatus {
+  id: string;
+  status: "running" | "committed" | "aborted";
+}
+
+export class ArangoTransaction {
+  isArangoTransaction: true = true;
+  private _connection: Connection;
+  id: string;
+
+  constructor(connection: Connection, id: string) {
+    this._connection = connection;
+    this.id = id;
+  }
+
+  get(): Promise<TransactionStatus> {
+    return this._connection.request(
+      {
+        path: `/_api/transaction/${this.id}`
+      },
+      res => res.body.result
+    );
+  }
+
+  commit(): Promise<TransactionStatus> {
+    return this._connection.request(
+      {
+        method: "PUT",
+        path: `/_api/transaction/${this.id}`
+      },
+      res => res.body.result
+    );
+  }
+
+  abort(): Promise<TransactionStatus> {
+    return this._connection.request(
+      {
+        method: "DELETE",
+        path: `/_api/transaction/${this.id}`
+      },
+      res => res.body.result
+    );
+  }
+
+  exec<T>(fn: () => Promise<T>): Promise<T> {
+    this._connection.setTransactionId(this.id);
+    return fn();
+  }
+}
