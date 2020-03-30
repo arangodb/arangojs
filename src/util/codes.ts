e4e8a09266929c97a3c40012c0174e64f35cf6fc
Alan Plum <me@pluma.io>
2020-03-30 13:08:51 +0200

    Remove unnecessary hidden tags

--- src/util/codes.ts
+++ src/util/codes.ts
@@ -1,14 +1,5 @@
-/** @hidden */
 export const ANALYZER_NOT_FOUND = 1202;
-
-/** @hidden */
 export const DOCUMENT_NOT_FOUND = 1202;
-
-/** @hidden */
 export const COLLECTION_NOT_FOUND = 1203;
-
-/** @hidden */
 export const DATABASE_NOT_FOUND = 1228;
-
-/** @hidden */
 export const GRAPH_NOT_FOUND = 1924;
