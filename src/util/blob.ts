fd8179f78db176edf6933b163415f2650f8b749a
Alan Plum <me@pluma.io>
2020-03-31 12:22:08 +0200

    Move Blob

---
+++ src/util/blob.ts
@@ -0,0 +1,8 @@
+export interface Blob {
+  readonly size: number;
+  readonly type: string;
+  arrayBuffer(): Promise<ArrayBuffer>;
+  slice(start?: number, end?: number, contentType?: string): Blob;
+  stream(): any;
+  text(): Promise<string>;
+}
--- src/util/types.ts
+++ src/util/types.ts
@@ -3,12 +3,3 @@ export type Errback<T> = (err: Error | null, result?: T) => void;
 export type Patch<T> = { [K in keyof T]?: T[K] | Patch<T[K]> };
 
 export type StrictObject<T> = { [K in keyof T]: T[K] };
-
-export interface Blob {
-  readonly size: number;
-  readonly type: string;
-  arrayBuffer(): Promise<ArrayBuffer>;
-  slice(start?: number, end?: number, contentType?: string): Blob;
-  stream(): any;
-  text(): Promise<string>;
-}
