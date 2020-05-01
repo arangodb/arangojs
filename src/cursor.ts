aae4d2b4f39e3794c3be8369de050345bb214432
Alan Plum <me@pluma.io>
2020-05-01 18:12:23 +0200

    Inline some cursor docs

--- src/cursor.ts
+++ src/cursor.ts
@@ -80,31 +80,36 @@ export class ArrayCursor<T = any> {
   }
 
   /**
-   * TODO
+   * The total number of documents in the query result. Only available if the
+   * `count` option was used.
    */
-  get count() {
+  get count(): number | undefined {
     return this._count;
   }
 
   /**
-   * TODO
+   * Whether the cursor has any remaining batches that haven't yet been
+   * fetched. If set to `false`, all batches have been fetched and no
+   * additional requests to the server will be made when consuming any
+   * remaining items from this cursor.
    */
   get hasMore(): boolean {
     return this._hasMore;
   }
 
   /**
-   * TODO
+   * Whether the cursor has more values. If set to `false`, the cursor has
+   * already been depleted and contains no more items.
    */
   get hasNext(): boolean {
     return this.hasMore || Boolean(this._result.length);
   }
 
   /**
-   * Enables use with `for await` to exhaust the cursor by asynchronously
+   * Enables use with `for await` to deplete the cursor by asynchronously
    * yielding every value in the cursor's remaining result set.
    *
-   * **Note**: If the result set spans multiple batches, any unfetched batches
+   * **Note**: If the result set spans multiple batches, any remaining batches
    * will only be fetched on demand. Depending on the cursor's TTL and the
    * processing speed, this may result in the server discarding the cursor
    * before it is fully depleted.
@@ -129,7 +134,14 @@ export class ArrayCursor<T = any> {
   }
 
   /**
-   * TODO
+   * Depletes the cursor, then returns an array containing all values in the
+   * cursor's remaining result list.
+   *
+   * @example
+   * ```js
+   * const cursor = await db.query(aql`FOR x IN 1..5 RETURN x`);
+   * const result = await cursor.all(); // [1, 2, 3, 4, 5]
+   * console.log(cursor.hasNext); // false
    */
   async all(): Promise<T[]> {
     await this._drain();
@@ -139,7 +151,22 @@ export class ArrayCursor<T = any> {
   }
 
   /**
-   * TODO
+   * Advances the cursor and returns the next value in the cursor's remaining
+   * result list, or `undefined` if the cursor has been depleted.
+   *
+   * **Note**: If the result set spans multiple batches, any remaining batches
+   * will only be fetched on demand. Depending on the cursor's TTL and the
+   * processing speed, this may result in the server discarding the cursor
+   * before it is fully depleted.
+   *
+   * @example
+   * ```js
+   * const cursor = await db.query(aql`FOR x IN 1..3 RETURN x`);
+   * const one = await cursor.next(); // 1
+   * const two = await cursor.next(); // 2
+   * const three = await cursor.next(); // 3
+   * const empty = await cursor.next(); // undefined
+   * ```
    */
   async next(): Promise<T | undefined> {
     while (!this._result.length && this.hasMore) {
@@ -152,7 +179,27 @@ export class ArrayCursor<T = any> {
   }
 
   /**
-   * TODO
+   * Advances the cursor and returns all remaining values in the cursor's
+   * current batch. If the current batch has already been exhausted, fetches
+   * the next batch from the server and returns it, or `undefined` if the
+   * cursor has been depleted.
+   *
+   * **Note**: If the result set spans multiple batches, any remaining batches
+   * will only be fetched on demand. Depending on the cursor's TTL and the
+   * processing speed, this may result in the server discarding the cursor
+   * before it is fully depleted.
+   *
+   * @example
+   * ```js
+   * const cursor = await db.query(
+   *   aql`FOR i IN 1..10 RETURN i`,
+   *   { batchSize: 5 }
+   * );
+   * const firstBatch = await cursor.nextBatch(); // [1, 2, 3, 4, 5]
+   * await cursor.next(); // 6
+   * const lastBatch = await cursor.nextBatch(); // [7, 8, 9, 10]
+   * console.log(cursor.hasNext); // false
+   * ```
    */
   async nextBatch(): Promise<any[] | undefined> {
     while (!this._result.length && this.hasMore) {
