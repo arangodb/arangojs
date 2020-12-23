bdf13168cf576ef24cf8c7dccde2666881daa979
Vadim <vadim@arangodb.com>
2020-12-24 01:52:48 +0300

    Update 28-accessing-analyzers.ts

--- src/test/28-accessing-analyzers.ts
+++ src/test/28-accessing-analyzers.ts
@@ -74,7 +74,7 @@ describe35("Accessing analyzers", function () {
           await analyzer.create({ type: "identity" });
           await db.waitForPropagation(
             { path: `/_api/analyzer/${analyzer.name}` },
-            30000
+            65000
           );
         })
       );
