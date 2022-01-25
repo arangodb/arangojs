a40a590458e9a2b68cffac00cffce0c84680250d
Alan Plum <me@pluma.io>
2022-01-25 09:08:34 +0100

    Use correct arangoVersion

--- src/test/_config.ts
+++ src/test/_config.ts
@@ -2,8 +2,15 @@ import { Config, LoadBalancingStrategy } from "../connection";
 
 const ARANGO_URL = process.env.TEST_ARANGODB_URL || "http://localhost:8529";
 const ARANGO_VERSION = Number(
-  process.env.ARANGO_VERSION || process.env.ARANGOJS_DEVEL_VERSION || 39999
+  process.env.ARANGO_VERSION || process.env.ARANGOJS_DEVEL_VERSION || 0
 );
+const ARANGO_RELEASE = process.env.ARANGO_RELEASE || "";
+let arangoVersion: number = 39999;
+if (ARANGO_VERSION) arangoVersion = ARANGO_VERSION;
+else if (ARANGO_RELEASE.includes(".")) {
+  const [major, minor] = ARANGO_RELEASE.split(".").map((v) => Number(v));
+  arangoVersion = (major * 100 + minor) * 100;
+}
 const ARANGO_LOAD_BALANCING_STRATEGY = process.env
   .TEST_ARANGO_LOAD_BALANCING_STRATEGY as LoadBalancingStrategy | undefined;
 
@@ -12,12 +19,12 @@ export const config: Config & {
 } = ARANGO_URL.includes(",")
   ? {
       url: ARANGO_URL.split(",").filter((s) => Boolean(s)),
-      arangoVersion: ARANGO_VERSION,
+      arangoVersion,
       precaptureStackTraces: true,
       loadBalancingStrategy: ARANGO_LOAD_BALANCING_STRATEGY || "ROUND_ROBIN",
     }
   : {
       url: ARANGO_URL,
-      arangoVersion: ARANGO_VERSION,
+      arangoVersion,
       precaptureStackTraces: true,
     };
