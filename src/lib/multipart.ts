5da3d8a0acb756e157f623a939db108ad10b5b9a
Alan Plum <pluma4345@users.noreply.github.com>
2022-10-12 12:54:43 +0200

    Update multi-part

--- src/lib/multipart.ts
+++ src/lib/multipart.ts
@@ -9,9 +9,13 @@ import { Readable } from "stream";
 import { Headers } from "../connection";
 
 declare class MultiPart {
-  append(key: string, value: Readable | Buffer | string): void;
-  getBoundary(): string;
-  stream(): Readable;
+  append(
+    field: string,
+    value: any,
+    options?: { filename?: string; contentType?: string }
+  ): this;
+  getHeaders(): Headers;
+  buffer(): Promise<Buffer>;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
@@ -35,40 +39,21 @@ export interface MultipartRequest {
 /**
  * @internal
  */
-export function toForm(fields: Fields): Promise<MultipartRequest> {
-  return new Promise((resolve, reject) => {
-    try {
-      const form = new Multipart();
-      for (const key of Object.keys(fields)) {
-        let value = fields[key];
-        if (value === undefined) continue;
-        if (
-          !(value instanceof Readable) &&
-          !(value instanceof global.Buffer) &&
-          (typeof value === "object" || typeof value === "function")
-        ) {
-          value = JSON.stringify(value);
-        }
-        form.append(key, value);
-      }
-      const stream = form.stream();
-      const bufs: Buffer[] = [];
-      stream.on("data", (buf) => bufs.push(buf as Buffer));
-      stream.on("end", () => {
-        bufs.push(Buffer.from("\r\n"));
-        const body = Buffer.concat(bufs);
-        const boundary = form.getBoundary();
-        const headers = {
-          "content-type": `multipart/form-data; boundary=${boundary}`,
-          "content-length": String(body.length),
-        };
-        resolve({ body, headers });
-      });
-      stream.on("error", (e) => {
-        reject(e);
-      });
-    } catch (e: any) {
-      reject(e);
+export async function toForm(fields: Fields): Promise<MultipartRequest> {
+  const form = new Multipart();
+  for (const key of Object.keys(fields)) {
+    let value = fields[key];
+    if (value === undefined) continue;
+    if (
+      !(value instanceof Readable) &&
+      !(value instanceof global.Buffer) &&
+      (typeof value === "object" || typeof value === "function")
+    ) {
+      value = JSON.stringify(value);
     }
-  });
+    form.append(key, value);
+  }
+  const body = await form.buffer();
+  const headers = form.getHeaders();
+  return { body, headers };
 }
