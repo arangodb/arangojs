b59a5ae95d5d048a74c71c6f9cee6242669e37a1
Alan Plum <pluma4345@users.noreply.github.com>
2022-10-23 18:04:27 +0200

    Improve web worker compat

--- src/lib/joinPath.web.ts
+++ src/lib/joinPath.web.ts
@@ -5,7 +5,12 @@
  * @internal
  */
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { join } = require("path-browserify");
+
 /**
  * @internal
  */
-export { join as joinPath } from "path";
+export function joinPath(...path: string[]): string {
+  return join(...path);
+}
--- src/lib/querystringify.web.ts
+++ src/lib/querystringify.web.ts
@@ -1,22 +1,29 @@
-import { ParsedUrlQueryInput } from "querystring";
-
-export function querystringify(obj: ParsedUrlQueryInput) {
-  const result = [] as string[];
+export function querystringify(
+  obj: Record<
+    string,
+    | string
+    | number
+    | boolean
+    | null
+    | undefined
+    | readonly (string | number | boolean | null | undefined)[]
+  >
+) {
+  let result = "";
   for (let [key, value] of Object.entries(obj)) {
     if (value === undefined) continue;
     key = encodeURIComponent(key);
     if (!Array.isArray(value)) {
       if (value === null) value = "";
       else value = encodeURIComponent(String(value));
-      result.push(`${key}=${value}`);
+      result += `&${key}=${value}`;
     } else {
       for (let item of value) {
-        if (item === undefined) continue;
-        if (item === null) item = "";
+        if (item == null) item = "";
         else item = encodeURIComponent(String(item));
-        result.push(`${key}=${item}`);
+        result += `&${key}=${item}`;
       }
     }
   }
-  return result.join(`&`);
+  return result.slice(1);
 }
