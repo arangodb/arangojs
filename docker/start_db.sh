33ec6408179c7d99b089e0d0e46ce0901bc9b621
bluepal-yaswanth-peravali <yaswanth.peravali@bluepal.com>
2026-05-28 11:00:27 +0530

    DE-1167 | runs CircleCI jobs on self hosted runners

--- docker/start_db.sh
+++ docker/start_db.sh
@@ -2,8 +2,8 @@
 
 # Configuration environment variables:
 #   STARTER_MODE:             (single|cluster|activefailover), default single
-#   DOCKER_IMAGE:             ArangoDB docker image, default docker.io/arangodb/enterprise:latest
-#   STARTER_DOCKER_IMAGE:     ArangoDB Starter docker image, default docker.io/arangodb/arangodb-starter:latest
+#   DOCKER_IMAGE:             ArangoDB docker image, default gcr.io/gcr-for-testing/arangodb/enterprise:latest
+#   STARTER_DOCKER_IMAGE:     ArangoDB Starter docker image, default docker.io/arangodb/arangodb-starter:0.18.5
 #   SSL:                      (true|false), default false
 #   ARANGO_LICENSE_KEY:       only required for ArangoDB Enterprise
 #
@@ -14,8 +14,8 @@
 # STARTER_MODE=cluster SSL=true ./start_db.sh
 
 STARTER_MODE=${STARTER_MODE:=single}
-DOCKER_IMAGE=${DOCKER_IMAGE:=docker.io/arangodb/enterprise:latest}
-STARTER_DOCKER_IMAGE=${STARTER_DOCKER_IMAGE:=docker.io/arangodb/arangodb-starter:latest}
+DOCKER_IMAGE=${DOCKER_IMAGE:=gcr.io/gcr-for-testing/arangodb/enterprise:latest}
+STARTER_DOCKER_IMAGE=${STARTER_DOCKER_IMAGE:=docker.io/arangodb/arangodb-starter:0.18.5}
 SSL=${SSL:=false}
 COMPRESSION=${COMPRESSION:=false}
 
